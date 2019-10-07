e225edca84b0d14289116ae61900d019511d4704
yann dupis <yann.dupis@gmail.com>
2019-10-07 16:14:19 -0700

    add test add for variables

--- test/variable_test.py
+++ test/variable_test.py
@@ -7,3 +7,10 @@ def test_send_get_variable(remote):
   x_ptr = x_to_give.send(remote)
   x_gotten = x_ptr.get()
   assert np.array_equal(x_to_give.numpy(), x_gotten.numpy())
+
+def test_add_variables(remote):
+  x = tf.Variable([3.0, 3.0]).send(remote)
+  y = tf.Variable([2.0, 2.0]).send(remote)
+  z_ptr = x + y
+  z = z_ptr.get()
+  assert np.array_equal(z.numpy(), [5., 5.])
