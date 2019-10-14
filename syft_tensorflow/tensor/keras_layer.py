b4215a5c6b0d1270dd04f6ac43371a82e9b268b0
yann dupis <yann.dupis@gmail.com>
2019-10-14 10:30:22 -0700

    clean-up kerasLayer class

--- syft_tensorflow/tensor/keras_layer.py
+++ syft_tensorflow/tensor/keras_layer.py
@@ -3,7 +3,6 @@ import weakref
 import tensorflow as tf
 
 import syft
-from syft.generic.frameworks.types import FrameworkTensor
 from syft.generic.frameworks.hook import hook_args
 from syft.generic.object import AbstractObject
 from syft.generic.pointers.object_pointer import ObjectPointer
@@ -13,21 +12,19 @@ from syft.exceptions import PureFrameworkTensorFoundError
 
 
 class KerasLayer(AbstractObject):
-    """Add methods to this tensor to have them added to every tf.Tensor object.
+    """Add methods to this keras layer object to have them added to every 
+    tf.keras.layers.Layer object.
 
-    This tensor is simply a more convenient way to add custom functions to
-    all TensorFlow tensor types. When you add a function to this tensor,
-    it will be added to EVERY native TF tensor type (i.e. tf.Tensor)
+    This Keras object is simply a more convenient way to add custom functions to
+    all TensorFlow Keras object types. When you add a function to this keras object,
+    it will be added to EVERY native Keras layer type (i.e. tf.keras.layers.Dense)
     automatically by the TensorFlowHook.
 
-    Note: all methods from AbstractTensor will also be included because this
-    tensor extends AbstractTensor. So, if you're looking for a method on
-    the native torch tensor API but it's not listed here, you might try
-    checking AbstractTensor.
+    Note: all methods from AbstractObject will also be included because this
+    object extends AbstractObject. So, if you're looking for a method on
+    the native tf.keras API but it's not listed here, you might try
+    checking AbstractObject.
     """
-
-    def has_child(self):
-        return hasattr(self, "child")
 
     def describe(self, description):
         self.description = description
@@ -47,38 +44,23 @@ class KerasLayer(AbstractObject):
 
     @property
     def tags(self):
-        if self.has_child():
-            return self.child.tags
-        else:
-            if not hasattr(self, "_tags"):
-                self._tags = None
-            return self._tags
+        if not hasattr(self, "_tags"):
+            self._tags = None
+        return self._tags
 
     @tags.setter
     def tags(self, new_tags):
-        if self.has_child():
-            if new_tags is not None:
-                self.child.tags = set(new_tags)
-            else:
-                self.child.tags = set()
-        else:
-            self._tags = new_tags
+        self._tags = new_tags
 
     @property
     def description(self):
-        if self.has_child():
-            return self.child.description
-        else:
-            if not hasattr(self, "_description"):
-                self._description = None
-            return self._description
+        if not hasattr(self, "_description"):
+            self._description = None
+        return self._description
 
     @description.setter
     def description(self, new_desc):
-        if self.has_child():
-            self.child.description = new_desc
-        else:
-            self._description = new_desc
+        self._description = new_desc
 
     def send(
         self, *location, inplace: bool = False, no_wrap=False, garbage_collect_data=True
@@ -100,23 +82,20 @@ class KerasLayer(AbstractObject):
             garbage_collect_data: argument passed down to create_pointer()
 
         Returns:
-            A tf.EagerTensor[PointerTensor] pointer to self. Note that this
-            object will likely be wrapped by a tf.EagerTensor wrapper.
+            A tf.keras.layers.Layer[ObjectPointer] pointer to self. Note that this
+            object will likely be wrapped by a ttf.keras.layers.Layer wrapper.
         """
 
         # If you send a pointer p1, you want the pointer to pointer p2
         # to control the garbage collection and not the remaining old
         # p1 (here self). Because if p2 is not GCed, GCing p1 shouldn't delete
-        # the remote tensor, but if you want to do so, as p2 is not GCed,
+        # the remote keras object, but if you want to do so, as p2 is not GCed,
         # you can still do `del p2`. This allows to chain multiple
         # .send().send() calls.
 
         if len(location) == 1:
 
             location = location[0]
-
-            if hasattr(self, "child") and isinstance(self.child, PointerTensor):
-                self.child.garbage_collect_data = False
 
             ptr = self.owner.send(
                 self, location, garbage_collect_data=garbage_collect_data
@@ -138,7 +117,6 @@ class KerasLayer(AbstractObject):
             self.ptr = weakref.ref(ptr)
 
             if inplace:
-                self.set_()  # TODO[jason]: pretty sure this is torch specific
                 self.child = ptr
                 return self
             else:
@@ -146,6 +124,8 @@ class KerasLayer(AbstractObject):
 
         else:
 
+            # TODO [Yann] check if we would want to send the keras
+            # object to several workers this way.
             children = list()
             for loc in location:
                 children.append(self.send(loc, no_wrap=True))
@@ -158,7 +138,7 @@ class KerasLayer(AbstractObject):
         return output
 
     def get(self, *args, inplace: bool = False, **kwargs):
-        """Requests the tensor/chain being pointed to, be serialized and return
+        """Requests the Keras object being pointed to, be serialized and return
             Args:
                 args: args to forward to worker
                 inplace: if true, return the same object instance,
@@ -166,22 +146,16 @@ class KerasLayer(AbstractObject):
                 kwargs: kwargs to forward to worker
             Raises:
                 GetNotPermittedError: Raised if
-                  get is not permitted on this tensor
+                  get is not permitted on this Keras object
         """
         # Transfer the get() to the child attribute which is a pointer
 
-        tensor = self.child.get(*args, **kwargs)
-
-        # Clean the wrapper
-        delattr(self, "child")
+        obj = self.child.get(*args, **kwargs)
 
         if inplace:
-            self.set_(tensor)  # TODO[jvmancuso]: torch-specific
-            if hasattr(tensor, "child"):
-                self.child = tensor.child
             return self
         else:
-            return tensor
+            return obj
 
     def create_pointer(
         self,
@@ -192,11 +166,11 @@ class KerasLayer(AbstractObject):
         ptr_id: (str or int) = None,
         garbage_collect_data: bool = True,
     ) -> ObjectPointer:
-        """Creates a pointer to the "self" torch.Tensor object.
+        """Creates a pointer to the "self" tf.keras.layers.Layer object.
 
         Returns:
-            A PointerTensor pointer to self. Note that this
-            object will likely be wrapped by a torch.Tensor wrapper.
+            A ObjectPointer pointer to self. Note that this
+            object will likely be wrapped by a tf.keras.layers.Layer wrapper.
         """
         if id_at_location is None:
             id_at_location = self.id
@@ -220,67 +194,31 @@ class KerasLayer(AbstractObject):
         return ptr
 
     def __str__(self) -> str:
-        if self.has_child():
-            if self.is_wrapper:
-                return "(Wrapper)>" + self.child.__str__()
-            else:
-                return type(self).__name__ + ">" + self.child.__str__()
-        else:
-            return self.native___str__()
+        return self.native___str__()
 
     def __repr__(self) -> str:
-        if self.has_child():
-            if self.is_wrapper:
-                return "(Wrapper)>" + self.child.__str__()
-            else:
-                return type(self).__name__ + ">" + self.child.__repr__()
-        else:
-            out = self.native___repr__()
+        out = self.native___repr__()
 
-            big_repr = False
+        big_repr = False
 
-            if self.tags is not None and len(self.tags):
-                big_repr = True
-                out += "\n\tTags: "
-                for tag in self.tags:
-                    out += str(tag) + " "
+        if self.tags is not None and len(self.tags):
+            big_repr = True
+            out += "\n\tTags: "
+            for tag in self.tags:
+                out += str(tag) + " "
 
-            if self.description is not None:
-                big_repr = True
-                out += (
-                    "\n\tDescription: " + str(self.description).split("\n")[0] + "..."
-                )
+        if self.description is not None:
+            big_repr = True
+            out += (
+                "\n\tDescription: " + str(self.description).split("\n")[0] + "..."
+            )
 
-            if big_repr:
-                out += "\n\tShape: " + str(self.shape.as_list())
-
-            return out
+        return out
 
     @classmethod
     def handle_func_command(cls, command):
         """
-        Operates as a router for functions. A function call always starts
-        by being handled here and 3 scenarii must be considered:
-
-        Real Torch tensor:
-            The arguments of the function are real tensors so we should
-            run the native torch command
-
-        Torch wrapper:
-            The arguments are just wrappers at the top of a chain
-            (ex: wrapper>LoggingTensor>Torch tensor), so just forward
-            the instruction to the next layer type in the chain (in
-            the example above to LoggingTensor.handle_func_command),
-            get the response and replace a wrapper on top of all tensors
-            found in the response.
-
-        Syft Tensor:
-            The arguments are syft tensors of same type: this can happen
-            if at any node of the chain where some function is forwarded,
-            the handle_func_command modify the function and make a new
-            call but keeps the arguments "un-wrapped". Making a new call
-            means that by default the command is treated here in the
-            global router.
+        Instantiate the native Keras object.
 
         :param command: instruction of a function command: (command name,
         <no self>, arguments[, kwargs])
@@ -288,46 +226,18 @@ class KerasLayer(AbstractObject):
         """
         cmd, _, args, kwargs = command
 
-        try:  # will work if tensors are wrappers
+        # TODO: clean this line
+        cmd_split = cmd.split(".")
+        cmd_path = cmd_split[:-1]
+        cmd_name = cmd_split[-1]
+        cmd = "syft.local_worker.hook." + ".".join(cmd_path) + ".native_" + cmd_name
 
-            # Replace all torch tensor with their child attribute
-            # Note that we return also args_type which helps handling case 3 in the docstring
-            new_args, new_kwargs, new_type, args_type = hook_args.unwrap_args_from_function(
-                cmd, args, kwargs, return_args_type=True
-            )
-            # This handles case 3: it redirects the command to the appropriate class depending
-            # of the syft type of the arguments and returns
-            if args_type not in FrameworkTensor:
-                return args_type.handle_func_command(command)
-
-            # build the new command
-            new_command = (cmd, None, new_args, new_kwargs)
-            # Send it to the appropriate class and get the response
-            response = new_type.handle_func_command(new_command)
-            # Put back the wrappers where needed
-            response = hook_args.hook_response(cmd, response, wrap_type=args_type)
-        except PureFrameworkTensorFoundError:  # means that it's not a wrapper but a pure tensor
-
-            # Check that the function has not been overwritten
-            try:
-                # Try to get recursively the attributes in cmd = "<attr1>.<attr2>.<attr3>..."
-                command = cls.rgetattr(cls, cmd)
-                return command(*args, **kwargs)
-            except AttributeError:
-                pass
-
-            # TODO: clean this line
-            cmd_split = cmd.split(".")
-            cmd_path = cmd_split[:-1]
-            cmd_name = cmd_split[-1]
-            cmd = "syft.local_worker.hook." + ".".join(cmd_path) + ".native_" + cmd_name
-
-            # Run the native function with the new args
-            # Note the the cmd should already be checked upon reception by the worker
-            # in the execute_command function
-            if isinstance(args, tuple):
-                response = eval(cmd)(*args, **kwargs)
-            else:
-                response = eval(cmd)(args, **kwargs)
+        # Run the native function with the new args
+        # Note the the cmd should already be checked upon reception by the worker
+        # in the execute_command function
+        if isinstance(args, tuple):
+            response = eval(cmd)(*args, **kwargs)
+        else:
+            response = eval(cmd)(args, **kwargs)
 
         return response
