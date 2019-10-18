664b33a5ffdd01221ff3c02a2063a7491229ae0d
Benjamin DeCoste <bendecoste@gmail.com>
2019-10-17 22:56:14 -0300

    added a sigmoid test (#26)

--- syft_tensorflow/keras/tf_keras_fun_test.py
+++ syft_tensorflow/keras/tf_keras_fun_test.py
@@ -4,16 +4,23 @@ import tensorflow as tf
 import syft
 
 
-def test_keras_activations_fn():
-    hook = syft.TensorFlowHook(tf)
-    bob = syft.VirtualWorker(hook, id="bob")
+def test_keras_activations_fn(remote):
 
     x_to_give = tf.constant([-2.0, 3.0, 5.0])
     expected = tf.keras.activations.relu(x_to_give)
 
-    x_ptr = x_to_give.send(bob)
+    x_ptr = x_to_give.send(remote)
 
     relu_ptr = tf.keras.activations.relu(x_ptr)
     actual = relu_ptr.get()
 
     assert tf.math.equal(actual, expected).numpy().all()
+
+def test_keras_sigmoid(remote):
+
+  expected = tf.keras.activations.sigmoid(tf.constant([1.0, 1.0]))
+
+  x = tf.constant([1.0, 1.0]).send(remote)
+  y = tf.keras.activations.sigmoid(x).get()
+
+  assert tf.math.equal(y, expected).numpy().all()
